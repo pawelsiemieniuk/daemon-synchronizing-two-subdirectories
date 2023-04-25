a7eeaabd05faf1b343b4362ee9a122cd27e92df4
MarcelHerman <131613139+MarcelHerman@users.noreply.github.com>
2023-04-26 01:03:46 +0200

    Update 1 dir_op.c
    troche readDir() oddekumentowałem i przerobiłem, wymaga dalszej edycji

--- lib/dir_op.c
+++ lib/dir_op.c
@@ -20,38 +20,42 @@ typedef struct file_list {
 
 void readDir(f_list **list, char *pathname)
 {
-    // DIR *src_dir = opendir(pathname)
-    // struct dirent *src_file = readdir(src_dir)
+        DIR *src_dir = opendir(pathname);
+        struct dirent *src_file;
 
-    // while(src_file)
-    // {
-    // unsigned char file_type = src_file->d_type
-    // char filename[256] = src_file->d_name
+        while((src_file = readdir(src_dir)) != NULL)
+        {
+                if((strcmp(entry->d_name,".") == 0) || (strcmp(entry->d_name, "..") == 0)) {} //oznaczenia hierarchi plików
+                else
+                {
+                        unsigned char file_type = src_file->d_type;
+                        char filename[256] = src_file->d_name;
 
-    // struct stat *file_buff
-    // char *filepath = pathname + filename
-    // lstat(filepath, file_buff)
+                        struct stat *file_buff;     
+                        lstat(filepath, file_buff);
 
-    // struct f_info *file_i
-    // file_i->f_name = filename
-    // file_i->f_size = file_buff->st_size
-    // file_i->f_mtime = file_buff->st_mtim.tv_sec
+                        struct f_info *file_i;
+                        file_i->f_name = filename;
+                        file_i->f_size = file_buff->st_size;
+                        file_i->f_mtime = file_buff->st_mtim.tv_sec;
 
-    // struct f_list *src_list
-    // if(file_type = DT_REG) 
-    //    src_list->type = 0 
-    // else 
-    //    src_list->type = 1
-    // src_list->path = pathname
+                        struct f_list *src_list;
+                        if(file_type = DT_REG)
+                                src_list->type = 0;
+                        else 
+                                src_list->type = 1;
+                        src_list->path = pathname;
 
-    // if(src_list->type)
-    //    src_list->data.file_i = file_i
-    // else
-    //    src_list->data.sub_dir
-    //    readDir(filepath)
-
-    // src_file = readdir(src_dir)
-    // }
+                        if(src_list->type)
+                        {
+                                src_list->data.file_i = file_i;
+                                char *filepath = pathname + filename;
+                                readDir(list, filepath);
+                        }
+                      //  else
+                       //         src_list->data.sub_dir; tego tutaj nie czaje 
+                }
+        }
 }
 
 void copyDir(f_list **src_list){
