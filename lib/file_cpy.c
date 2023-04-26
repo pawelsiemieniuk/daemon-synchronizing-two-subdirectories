cb441e173dd6afd07ca35d0dffcfdcf3f06c2c47
ImFox <46004269+ImFoxo@users.noreply.github.com>
2023-04-26 19:18:28 +0200

    Update file_cpy.c

--- lib/file_cpy.c
+++ lib/file_cpy.c
@@ -4,13 +4,23 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-void cpy_mmap(file_info finf)
+void cpy_mmap(file_info finf, char *src_path)
 {
     void *src_map_pos, *dst_map_pos;
     size_t fsize = finf -> f_size;
+    char *fname = finf->f_name;
+    char *dst_path = calloc(strlen(DST_PATH) + strlen(src_path) - strlen(SRC_PATH) + strlen(fname) + 2);
 
-    unsigned int src_fd = open(SRC_PATH, O_RDONLY);
-    unsigned int dst_fd = open(DST_PATH, O_WRONLY | O_CREAT);
+    strcat(dst_path, DST_PATH);
+    for (int i = strlen(SRC_PATH); i < strlen(src_path); i++)
+    {
+        strcat(dst_path, src_path[i]);
+    }
+    strcat(dst_path, "/");
+    strcat(dst_path, fname);
+    
+    unsigned int src_fd = open(src_path, O_RDONLY);
+    unsigned int dst_fd = open(dst_path, O_WRONLY | O_CREAT);
 
     src_map_pos = mmap(0, fsize, PROT_READ, MAP_SHARED, src_fd, 0);
     dst_map_pos = mmap(0, fsize, PROT_WRITE, MAP_SHARED, dst_fd, 0);
@@ -22,15 +32,28 @@ void cpy_mmap(file_info finf)
 
     close(src_fd);
     close(dst_fd);
+
+    free(fname);
+    free(dst_path);
 }
 
-void cpy_normal(file_info finf)
+void cpy_normal(file_info finf, char *src_path)
 {
-    unsigned int src_fd = open(src, O_RDONLY);
-    unsigned int dst_fd = open(dst, O_WRONLY | O_CREAT);
-
-    size_t fsize = finf -> f_size;
     size_t *buffer = calloc(1, 1);
+    size_t fsize = finf -> f_size;
+    char *fname = finf->f_name;
+    char *dst_path = calloc(strlen(DST_PATH) + strlen(src_path) - strlen(SRC_PATH) + strlen(fname) + 2);
+
+    strcat(dst_path, DST_PATH);
+    for (int i = strlen(SRC_PATH); i < strlen(src_path); i++)
+    {
+        strcat(dst_path, src_path[i]);
+    }
+    strcat(dst_path, "/");
+    strcat(dst_path, fname);
+
+    unsigned int src_fd = open(src_path, O_RDONLY);
+    unsigned int dst_fd = open(dst_path, O_WRONLY | O_CREAT);
     
     read(src_fd, buffer, fsize);
     write(dst_fd, buffer, fsize);
